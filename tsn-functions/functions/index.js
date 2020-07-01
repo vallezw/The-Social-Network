0eddd0cc8b2c943a6b8d1687da54cf79ebc2e8f7
Valentin Zwerschke <v.zwerschke@mail.de>
2020-07-01 15:10:34 +0000

    Add Backend

--- tsn-functions/functions/index.js
+++ tsn-functions/functions/index.js
@@ -1,4 +1,8 @@
 const functions = require('firebase-functions');
+const admine = require('firebase-admin')
+
+var admin = require('firebase-admin');
+var app = admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +10,37 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+exports.getPosts = functions.https.onRequest((request, response) => {
+  admin.firestore().collection("Posts").get()
+  .then(data => {
+    let posts = []
+    data.forEach(doc => {
+      posts.push(doc.data())
+    })
+    return response.json(posts)
+  })
+  .catch((err) => console.error(err))
+})
+
+exports.createPost = functions.https.onRequest((request, response) => {
+  if (request.method !=="POST"){
+    return response.status(400).json({ error: "Method not allowed"})
+  }
+  const newPost = {
+    body: request.body.body,
+    userHandle: request.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  }
+
+  admin.firestore()
+    .collection("Posts")
+    .add(newPost)
+    .then(doc => {
+      response.json({ message: "document " + doc.id + " created successfully"})
+    })
+    .catch(err => {
+      response.status(500).json({ error: 'something went wrong'})
+      console.log(err);
+    })
+})
