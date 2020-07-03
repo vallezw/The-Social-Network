c1ff28cba49009a8a897da348342bdc4d4097095
Valentin Zwerschke <v.zwerschke@mail.de>
2020-07-03 17:52:19 +0000

    Add express

---
+++ tsn-functions/functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = [
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the post body',
+      createdAt: "2013-07-02T09:00:00",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+]
