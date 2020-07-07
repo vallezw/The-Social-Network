f36f63ee9247598680f595af7e57acb2d614a146
Valentin Zwerschke <v.zwerschke@mail.de>
2020-07-07 14:13:14 +0000

    Add usecredentials

--- tsn-functions/functions/index.js
+++ tsn-functions/functions/index.js
@@ -6,13 +6,17 @@ const exapp = express()
 const FBAuth = require('./util/fbAuth');
 
 const { getAllPosts, postOnePost } = require('./handlers/posts');
-const { signup, login, uploadImage } = require('./handlers/users');
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 // Post routes
 exapp.get('/posts', getAllPosts)
 exapp.post('/createPost', FBAuth, postOnePost)
 
+
 // Users route
 exapp.post('/signup', signup)
 exapp.post('/login', login)
+exapp.post('/user', FBAuth, addUserDetails)
+exapp.get('/user', FBAuth, getAuthenticatedUser)
 exapp.post('/user/image', FBAuth, uploadImage)
+
 exports.api = functions.region('europe-west1').https.onRequest(exapp)
