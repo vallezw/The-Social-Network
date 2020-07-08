de3ff40ad80899054e77389042e23045129892c3
Valentin Zwerschke <v.zwerschke@mail.de>
2020-07-08 15:32:35 +0000

    Add commenting functiojn

--- tsn-functions/functions/handlers/posts.js
+++ tsn-functions/functions/handlers/posts.js
@@ -38,3 +38,55 @@ exports.postOnePost =  (request, response) => {
       console.error(err);
     })
 }
+// Fetch one scream
+exports.getPost = (req, res) => {
+  let postData = {}
+  db.doc(`/Posts/${req.params.postId}`).get()
+    .then(doc => {
+      if(!doc.exists){
+        return res.status(404).json({error: 'Post not found'})
+      }
+      postData = doc.data()
+      postData.postId = doc.id
+      return db.collection('Comments').orderBy('createdAt', 'desc').where('postId', '==', req.params.postId).get()
+    })
+    .then(data => {
+      postData.comments = []
+      data.forEach(doc => {
+        postData.comments.push(doc.data())
+      })
+      return res.json(postData)
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({error: err.code})
+    })
+}
+
+// Comment on a comment
+exports.commentOnPost = (req, res) => {
+  if(req.body.body.trim() === '') return res.status(400).json({ error: 'Must not be empty.'})
+
+  const newComment = {
+    body: req.body.body,
+    createdAt: new Date().toISOString(),
+    postId: req.params.postId,
+    userHandle: req.user.handle,
+    userImage: req.user.imageUrl
+  }
+
+  db.doc(`/Posts/${req.params.postId}`).get()
+    .then(doc => {
+      if(!doc.exists){
+        return res.status(404).json({error: 'Post not found'})
+      }
+      return db.collection('Comments').add(newComment)
+    })
+    .then(() => {
+      res.json(newComment)
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({error: 'Something went wrong'})
+    })
+}
--- tsn-functions/functions/util/fbAuth.js
+++ tsn-functions/functions/util/fbAuth.js
@@ -20,10 +20,11 @@ module.exports = (request, response, next) => {
     })
     .then(data => {
       request.user.handle = data.docs[0].data().handle
+      request.user.imageUrl = data.docs[0].data().imageUrl
       return next()
     })
     .catch(err => {
-      console.log('Error while verifying token', err);
+      console.error('Error while verifying token:\n', err);
       return response.status(403).json(err)
     })
 }
