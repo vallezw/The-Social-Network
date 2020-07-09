b3b4e361b9d5160e5a878c605588d0d98728b408
Valentin Zwerschke <v.zwerschke@mail.de>
2020-07-09 20:43:20 +0000

    ADd posts

--- tsn-react/src/pages/Home.js
+++ tsn-react/src/pages/Home.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid'
+import axios from 'axios'
+
+// Components
+import Post from '../components/Post'
 
 class Home extends Component {
+    
+    state = {
+        posts: null
+    }
+
+    componentDidMount(){
+        axios.get('/posts')
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    posts: res.data
+                })
+            })
+            .catch(err => console.error(err))
+    }
+    
     render() {
+        let recentPostsMarkUp = this.state.posts ? (
+        this.state.posts.map(post => <Post post={post}/>)
+        ): <p>Loading...</p>
+
         return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
+            <Grid container spacing={2}>
+                <Grid item sm={8} xs={12}>
+                    {recentPostsMarkUp}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile,...</p>
+                </Grid>
+            </Grid>
         )
     }
 }
