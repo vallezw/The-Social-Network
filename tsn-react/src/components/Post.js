689883ab205e6693848ec93dcab25baa53a06754
Valentin Zwerschke <v.zwerschke@mail.de>
2020-07-09 20:57:46 +0000

    Add dayjs

--- tsn-react/src/components/Post.js
+++ tsn-react/src/components/Post.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 import { Link } from 'react-router-dom'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
 // MUI Imports
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -23,6 +26,7 @@ const styles = {
 
 export class Post extends Component {
     render() {
+        dayjs.extend(relativeTime)
         const { classes, post: { body, createdAt, userImage, userHandle, postId, likeCount, commentCount} } = this.props
         return (
             <Card className={classes.card}>
@@ -31,7 +35,7 @@ export class Post extends Component {
                 title="Profile Image" className={classes.image}/>
                 <CardContent className={classes.content}>
                     <Typography variant="h5" component={Link} to={`/users/${userHandle}`} color="primary">{userHandle}</Typography>
-                    <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+                    <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
                     <Typography variant="body1">{body}</Typography>
                 </CardContent>
             </Card>
