0fc9e317b33c80557af3312e411b07ed55b9aabd
Valentin Zwerschke <v.zwerschke@mail.de>
2020-07-10 16:59:56 +0000

    Edit Token

--- tsn-react/src/App.js
+++ tsn-react/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
+import themeFile from './util/theme'
+import jwtDecode from 'jwt-decode'
 
-// Navbar
+// Components
 import Navbar from './components/Navbar'
+import AuthRoute from './util/AuthRoute'
 // Pages
 import home from './pages/home'
 import login from './pages/login'
@@ -12,16 +15,31 @@ import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 //import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#FF7F50',
-    },
-    secondary: {
-      main: '#FF8C00',
-    },
-  },
-})
+const theme = createMuiTheme(themeFile)
+
+// Token stuff
+let authenticated
+const token = localStorage.FBIdToken
+if(token){
+  const decodedToken = jwtDecode(token)
+  const unixExpDate = decodedToken.exp
+  const dateExpDate = new Date(unixExpDate * 1000)
+  console.log(dateExpDate > Date.now());
+  
+  if(dateExpDate > Date.now()){
+    window.location.href = '/login'
+    //console.log(decodedToken);
+    
+    authenticated = false
+    //console.log('authenticated is false');
+  }
+  else {
+    //console.log('authenticated is true');
+    
+    authenticated = true
+  }
+}
+
 
 function App() {
   return (
@@ -32,8 +50,8 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={home} />
-              <Route exact path="/login" component={login} />
-              <Route exact path="/signup" component={signup} />
+              <AuthRoute exact path="/login" comp={login} authenticated={authenticated}/>
+              <AuthRoute exact path="/signup" comp={signup} authenticated={authenticated} />
             </Switch>
           </div>
         </Router>
---
+++ tsn-react/src/util/theme.js
@@ -0,0 +1,42 @@
+export default {
+    palette: {
+      primary: {
+        main: '#FF7F50',
+      },
+      secondary: {
+        main: '#FF8C00',
+      },  
+    },
+    typography: {
+      useNextVariants: true
+    },
+    spreadThis: {
+      form: {
+        textAlign: 'center'
+      },
+      image: {
+          margin: '10px auto 10px auto',
+          width: '150px',
+          height: 'auto'
+      },
+      pageTitle: {
+          margin: '10px auto 10px auto',
+          fontFamily: 'Roboto'
+      },
+      textField: {
+          margin: '10px auto 10px auto'
+      },
+      button: {
+          marginTop: 20,
+          position: 'relative'
+      },
+      customError: {
+          color: 'tomato',
+          fontSize: '0.8rem',
+          marginTop: 7
+      },
+      progress: {
+          position: 'absolute'
+      }
+    }
+  }
