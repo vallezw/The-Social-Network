3c1ad1f5d30bd034825bf79b0c53a12890ef045a
Valentin Zwerschke <v.zwerschke@mail.de>
2020-07-10 14:53:40 +0000

    Add LogiN form

--- tsn-react/src/App.js
+++ tsn-react/src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 // Navbar
 import Navbar from './components/Navbar'
 // Pages
-import Home from './pages/Home'
-import LogIn from './pages/LogIn'
-import SignUp from './pages/SignUp'
+import home from './pages/home'
+import login from './pages/login'
+import signup from './pages/signup'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 //import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import { MuiThemeProvider } from '@material-ui/core/styles';
@@ -31,9 +31,9 @@ function App() {
           <Navbar />
           <div className="container">
             <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={LogIn} />
-              <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
             </Switch>
           </div>
         </Router>
