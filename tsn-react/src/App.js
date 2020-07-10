cf1a79a7e9464477f44b63c643c1af33a12893d7
Valentin Zwerschke <v.zwerschke@mail.de>
2020-07-10 22:38:51 +0000

    Connect Signup wiht redux

--- tsn-react/src/App.js
+++ tsn-react/src/App.js
@@ -6,6 +6,8 @@ import jwtDecode from 'jwt-decode'
 //Redux
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import { SET_AUTHENTICATED } from './redux/types'
+import { logoutUser, getUserData } from './redux/actions/userActions'
 
 // Components
 import Navbar from './components/Navbar'
@@ -17,23 +19,25 @@ import signup from './pages/signup'
 //import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import axios from 'axios';
 
 
 const theme = createMuiTheme(themeFile)
 
 // Token stuff
-let authenticated
 const token = localStorage.FBIdToken
 if(token){
   const decodedToken = jwtDecode(token)
   const ts = Date.now()
   const currentTime = Math.floor(ts/1000) - 7200
   if(decodedToken.exp < currentTime){
+    store.dispatch(logoutUser())
     window.location.href = '/login'
-    authenticated = false
   }
   else {
-    authenticated = true
+    store.dispatch({ type: SET_AUTHENTICATED })
+    axios.defaults.headers.common['Authorization'] = token
+    store.dispatch(getUserData())
   }
 }
 
@@ -47,8 +51,8 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={home} />
-              <AuthRoute exact path="/login" comp={login} authenticated={authenticated}/>
-              <AuthRoute exact path="/signup" comp={signup} authenticated={authenticated} />
+              <AuthRoute exact path="/login" comp={login} />
+              <AuthRoute exact path="/signup" comp={signup} />
             </Switch>
           </div>
         </Router>
