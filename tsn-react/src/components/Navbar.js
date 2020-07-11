4ae554ee10e3974557ca60dcda35f5351da3d7c5
Valentin Zwerschke <v.zwerschke@mail.de>
2020-07-11 00:57:25 +0000

    Add Navbar stuff

--- tsn-react/src/components/Navbar.js
+++ tsn-react/src/components/Navbar.js
@@ -1,34 +1,61 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button';
 
+// Nav Stuff
 import Navbar from 'react-bootstrap/Navbar'
-//import Form from 'react-bootstrap/Form'
-//import FormControl from 'react-bootstrap/FormControl'
 import Nav from 'react-bootstrap/Nav'
-//import Button from 'react-bootstrap/Button'
-//import NavDropdown from 'react-bootstrap/NavDropdown'
+// Icons
+import Notifications from '@material-ui/icons/Notifications'
+import AddIcon from '@material-ui/icons/Add';
+import { Tooltip } from '@material-ui/core';
 
-export default function Navigationbar(){
-  return(
-    <Navbar variant="light"expand="lg">
-      <Navbar.Brand href="/">The Social Network</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="login">Log In</Nav.Link>
-          <Nav.Link href="signup">Sign Up</Nav.Link>
-          {/* <NavDropdown title="Addon for Browser" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Chrome</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.2">Firefox</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.3">Opera</NavDropdown.Item>
-          </NavDropdown> */}
-        </Nav>
-        {/* <Form inline>
-          <FormControl type="text" placeholder="Search for Video" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form> */}
-      </Navbar.Collapse>
-    </Navbar>
-  )
+class Navigationbar extends Component{
+  render(){
+    const { authenticated } = this.props
+    return(
+      <Navbar variant="light"expand="lg">
+        <Navbar.Brand href="/">The Social Network</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        { authenticated ? (
+          <Fragment>
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Button>
+                <Tooltip title="Create a Post!">
+                  <AddIcon color="primary"/>
+                </Tooltip>
+              </Button>
+              <Button>
+                <Tooltip title="Notifications">
+                  <Notifications color="primary"/>
+                </Tooltip>
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+          </Fragment>
+        ): (
+          <Fragment>
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="login">Log In</Nav.Link>
+              <Nav.Link href="signup">Sign Up</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Fragment>
+        )}
+      </Navbar>
+    )
+  }
 }
+
+Navigationbar.propTypes = {
+  authenticated: PropTypes.bool.isRequired
+}
+
+const mapStateToProps = state => ({
+  authenticated: state.user.authenticated
+})
+
+export default connect(mapStateToProps)(Navigationbar)
